9072cd70e71519212a0a7ff08a1eb7ac9e6a9ba7
1982carlosvillalobos-creator <1982.carlos.villalobos@gmail.com>
2025-09-03 06:41:51 -0700

    Create script.js

---
+++ _estimator/script.js
@@ -0,0 +1,17 @@
+async function loadAllData() {
+  try {
+    const res1 = await fetch('../prices.json');
+    prices = await res1.json();
+
+    const res2 = await fetch('data/labor-rates.json');
+    laborRates = await res2.json();
+
+    const res3 = await fetch('data/permits.json');
+    permits = await res3.json();
+
+    const res4 = await fetch('data/materials-db.json');
+    materialsDB = await res4.json();
+  } catch (e) {
+    console.error("Error cargando datos", e);
+  }
+}
